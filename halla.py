9abf8ebd57c78f5208463fe154b6bfe929fc5ef6
tuvahorgen <98455476+tuvahorgen@users.noreply.github.com>
2024-10-16 15:26:58 +0100

    Update halla.py

--- halla.py
+++ halla.py
@@ -3,6 +3,12 @@ import random
 
 solver = pywraplp.Solver.CreateSolver('SCIP')
 
+##Define the indexes
+#i = park
+#l = transmission cable
+
+## Define the constants
+
 #Production of electricty from a wind turbine in park i
 ElectricityProduction = 0 
 
@@ -18,9 +24,18 @@ transmissionCablea = []
 #Cost for tranmission cable L for wind turbine park i
 costPerCable = []
 
-electricityDemand = #noe
+#Transmission capacity of cable L
+transmissionCapacity = 0#noe
 
-transferCapacityCable = 
+#Requirement for quantum of delivered power
+deliveredPower = 0
+
+##Define the variables
+#Number of windmills in park i, i = 1,2,3,4,...,n
+X_i = 0 
+
+#1 if cable l is used for park i, 0 otherwise, l = 1,2,3,4,...,L
+S_il = 0
 
 # Generate random annual kWh production value
 random_kwh = random.uniform(21_000_000, 52_500_000)
@@ -29,5 +44,3 @@ print(f"Random annual kWh production: {random_kwh}")
 # Generate random daily kWh production value
 random_daily_kwh = random.uniform(57_500, 144_000)
 print(f"Random daily kWh production: {random_daily_kwh}")
-
-
